7908bf0860960f7a40044e7e0a06525e8777dec1
Konstantin <knetsov91@gmail.com>
2025-02-28 15:13:14 +0200

    Add RegisterRequest DTO.

---
+++ src/main/java/restaurant/com/restaurant/web/dto/RegisterRequest.java
@@ -0,0 +1,38 @@
+package restaurant.com.restaurant.web.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String email;
+    private String password;
+    private String passwordConfirmation;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+}
