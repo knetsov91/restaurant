a65e79d9f4feb35db7b5681d73b61a8b6100efc7
Konstantin <knetsov91@gmail.com>
2025-03-03 00:02:18 +0200

    Add login DTO.

---
+++ src/main/java/restaurant/com/restaurant/web/dto/LoginRequest.java
@@ -0,0 +1,17 @@
+package restaurant.com.restaurant.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String email;
+
+    private String password;
+}
