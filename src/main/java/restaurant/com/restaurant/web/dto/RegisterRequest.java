19a21e2b45e6d63c7bcde0c8d84d85b3b4cf814a
Konstantin <knetsov91@gmail.com>
2025-03-03 00:52:14 +0200

    Add validations to register DTO.

--- src/main/java/restaurant/com/restaurant/web/dto/RegisterRequest.java
+++ src/main/java/restaurant/com/restaurant/web/dto/RegisterRequest.java
@@ -1,44 +1,28 @@
 package restaurant.com.restaurant.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
+@Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
+    @Email(message = "enter valid email")
+    @NotNull
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
+
+    @NotBlank
+    @Size(min = 6)
     private String passwordConfirmation;
 
-    public RegisterRequest() {
-    }
 
-    public RegisterRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordConfirmation() {
-        return passwordConfirmation;
-    }
-
-    public void setPasswordConfirmation(String passwordConfirmation) {
-        this.passwordConfirmation = passwordConfirmation;
-    }
 }
