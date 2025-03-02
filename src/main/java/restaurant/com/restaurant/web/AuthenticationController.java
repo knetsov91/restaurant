d202b092e21e27b2ca22c8d4a7450130a4d301c0
Konstantin <knetsov91@gmail.com>
2025-03-03 00:21:12 +0200

    Add POST /register endpoint.

--- src/main/java/restaurant/com/restaurant/web/AuthenticationController.java
+++ src/main/java/restaurant/com/restaurant/web/AuthenticationController.java
@@ -1,19 +1,46 @@
 package restaurant.com.restaurant.web;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import restaurant.com.restaurant.user.service.UserService;
+import restaurant.com.restaurant.web.dto.LoginRequest;
 import restaurant.com.restaurant.web.dto.RegisterRequest;
 
 @Controller
 public class AuthenticationController {
 
+    private final UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @GetMapping("/register")
     public ModelAndView register() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("register");
-        modelAndView.addObject("registerRequest", new RegisterRequest());
+        modelAndView.addObject("registerRequest", RegisterRequest.builder().build());
         return modelAndView;
     }
+
+    @PostMapping("/register")
+    public ModelAndView registerPost(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("register");
+        }
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/");
+    }
+
+
 }
