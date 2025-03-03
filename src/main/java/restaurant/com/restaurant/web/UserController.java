69d9e479e283348bb232fef47aaeac255319fe2f
Konstantin <knetsov91@gmail.com>
2025-03-03 16:03:22 +0200

    Add establish session after user login.

---
+++ src/main/java/restaurant/com/restaurant/web/UserController.java
@@ -0,0 +1,16 @@
+package restaurant.com.restaurant.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getUsers() {
+        return ResponseEntity.ok("Users");
+    }
+}
