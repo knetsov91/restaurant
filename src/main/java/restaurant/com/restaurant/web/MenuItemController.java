74ea9e59dff5b85e374b70946407d09c9f13fbb4
Konstantin <knetsov91@gmail.com>
2025-03-03 23:28:19 +0200

    Add GET /items/create for render  creating menu item form.

--- src/main/java/restaurant/com/restaurant/web/MenuItemController.java
+++ src/main/java/restaurant/com/restaurant/web/MenuItemController.java
@@ -1,10 +1,12 @@
 package restaurant.com.restaurant.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import restaurant.com.restaurant.web.dto.MenuItemsRequest;
 import restaurant.com.restaurant.web.dto.RegisterRequest;
 
 import java.util.List;
@@ -13,13 +15,24 @@ import java.util.List;
 @RequestMapping("/menu")
 public class MenuItemController {
 
+
     @GetMapping("/items")
     public ModelAndView getItems() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("menu-items");
         modelAndView.addObject("items", List.of());
-        modelAndView.addObject("registerRequest", RegisterRequest.builder().build());
         return modelAndView;
 
     }
+
+    @GetMapping("/items/create")
+    public ModelAndView createItem() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("menu-items-create");
+        modelAndView.addObject("items", List.of());
+        modelAndView.addObject("menuItemsRequest", MenuItemsRequest.builder().build());
+        return modelAndView;
+
+    }
+
 }
