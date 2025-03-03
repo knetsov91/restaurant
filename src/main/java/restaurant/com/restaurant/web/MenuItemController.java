cd6dcbcc66c70ea799c12ae49ea6bf66df08eb48
Konstantin <knetsov91@gmail.com>
2025-03-03 22:29:28 +0200

    Add GET /menu/items endpoint.

---
+++ src/main/java/restaurant/com/restaurant/web/MenuItemController.java
@@ -0,0 +1,25 @@
+package restaurant.com.restaurant.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import restaurant.com.restaurant.web.dto.RegisterRequest;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuItemController {
+
+    @GetMapping("/items")
+    public ModelAndView getItems() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("menu-items");
+        modelAndView.addObject("items", List.of());
+        modelAndView.addObject("registerRequest", RegisterRequest.builder().build());
+        return modelAndView;
+
+    }
+}
