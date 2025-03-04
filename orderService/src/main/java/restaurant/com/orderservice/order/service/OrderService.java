bd064d50f1d38f282d5b22dececaec3d2141385d
Konstantin <knetsov91@gmail.com>
2025-03-04 13:23:58 +0200

    Add createOrder service method.

--- orderService/src/main/java/restaurant/com/orderservice/order/service/OrderService.java
+++ orderService/src/main/java/restaurant/com/orderservice/order/service/OrderService.java
@@ -1,5 +1,7 @@
 package restaurant.com.orderservice.order.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import restaurant.com.orderservice.order.model.Order;
@@ -10,10 +12,11 @@ import restaurant.com.orderservice.web.dto.OrderResponse;
 import restaurant.com.orderservice.web.mapper.DtoMapper;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
-
+    private final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
 
     @Autowired
@@ -27,6 +30,14 @@ public class OrderService {
                 .stream()
                 .map(DtoMapper::mapOrderToOrderResponse)
                 .toList();
+    }
+
+    public UUID createOrder(CreateOrderRequest createOrderRequest) {
+
+        Order order = DtoMapper.mapOrderToOrderResponse(createOrderRequest);
+        orderRepository.save(order);
+        LOGGER.info("Order {} created: {}", order.getId(), order.getCreatedAt());
+        return order.getId();
     }
 
 }
