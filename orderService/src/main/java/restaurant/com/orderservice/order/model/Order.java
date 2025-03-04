dcd1c2ea893c872f3f7ceda1de3ea9809ba77358
Konstantin <knetsov91@gmail.com>
2025-03-04 10:53:06 +0200

    Add Order entity.

---
+++ orderService/src/main/java/restaurant/com/orderservice/order/model/Order.java
@@ -0,0 +1,37 @@
+package restaurant.com.orderservice.order.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import restaurant.com.restaurant.order.model.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "`order`")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String tableName;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false)
+    private LocalDateTime finisedAt;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+
+}
