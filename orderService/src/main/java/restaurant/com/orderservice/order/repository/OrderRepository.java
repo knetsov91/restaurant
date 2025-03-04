d903e58f0daf91c32fa434e1cd4b66daf2183970
Konstantin <knetsov91@gmail.com>
2025-03-04 15:42:22 +0200

    Add getOrderByStatus repository method that use HQL.

--- orderService/src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
+++ orderService/src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package restaurant.com.orderservice.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import restaurant.com.orderservice.order.model.Order;
+import restaurant.com.orderservice.order.model.OrderStatus;
 
 import javax.swing.text.html.Option;
 import java.util.List;
@@ -13,4 +15,7 @@ import java.util.UUID;
 public interface OrderRepository extends JpaRepository<Order, UUID> {
 
     Optional<List<Order>> findByWaiter(UUID orderId);
+
+    @Query("FROM Order o WHERE o.orderStatus = :status")
+    List<Order> getOrderByStatus(OrderStatus status);
 }
