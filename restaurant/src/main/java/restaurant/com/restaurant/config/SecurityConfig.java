ca9196f83ed3675116a2fe182855a77e006bb353
Konstantin <knetsov91@gmail.com>
2025-03-27 16:08:59 +0200

    Add Spring Security and add authenticated routes.

--- restaurant/src/main/java/restaurant/com/restaurant/config/SecurityConfig.java
+++ restaurant/src/main/java/restaurant/com/restaurant/config/SecurityConfig.java
@@ -1,35 +1,29 @@
 package restaurant.com.restaurant.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import restaurant.com.restaurant.security.SecurityInterceptor;
 
+
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
 
-    private SecurityInterceptor securityInterceptor;
-
-    @Autowired
-    public SecurityConfig(SecurityInterceptor securityInterceptor) {
-        this.securityInterceptor = securityInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(securityInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/js/**", "/images/**");
-    }
-
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(matchers ->
+                    matchers.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .requestMatchers("/", "/register", "/login").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .build();
     }
-
-
 }
