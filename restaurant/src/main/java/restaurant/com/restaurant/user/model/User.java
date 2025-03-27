c2ff8fa1a1f644250c0d510f70a448505aa097c4
Konstantin <knetsov91@gmail.com>
2025-03-27 12:29:14 +0200

    Add field to User entity.

--- restaurant/src/main/java/restaurant/com/restaurant/user/model/User.java
+++ restaurant/src/main/java/restaurant/com/restaurant/user/model/User.java
@@ -2,11 +2,8 @@ package restaurant.com.restaurant.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import restaurant.com.restaurant.order.model.UserRole;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 
 @Builder
 @AllArgsConstructor
@@ -32,13 +29,13 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    private boolean isActive;
+
     @Column
     private LocalDateTime createdOn;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole role;
-
-
 
 }
