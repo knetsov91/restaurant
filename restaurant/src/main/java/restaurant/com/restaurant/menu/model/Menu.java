9fdd10385bb945a115d5d05e944a8a18db5a5271
Konstantin <knetsov91@gmail.com>
2025-03-27 13:19:33 +0200

    Change Menu entity.

--- restaurant/src/main/java/restaurant/com/restaurant/menu/model/Menu.java
+++ restaurant/src/main/java/restaurant/com/restaurant/menu/model/Menu.java
@@ -2,6 +2,7 @@ package restaurant.com.restaurant.menu.model;
 
 import jakarta.persistence.*;
 import restaurant.com.restaurant.menuitem.model.MenuItem;
+import restaurant.com.restaurant.restaurant.model.Restaurant;
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
@@ -20,4 +21,8 @@ public class Menu {
 
     @OneToMany(mappedBy = "menu")
     private Set<MenuItem> menuItems;
+
+    @ManyToOne
+    @JoinColumn(name="restaurant_id")
+    private Restaurant restaurant;
 }
