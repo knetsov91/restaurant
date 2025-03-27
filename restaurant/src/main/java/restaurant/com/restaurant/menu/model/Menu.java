6d9c39b418d74580713b946939af51009294bb6e
Konstantin <knetsov91@gmail.com>
2025-03-27 10:24:15 +0200

    Add Menu entity.

---
+++ restaurant/src/main/java/restaurant/com/restaurant/menu/model/Menu.java
@@ -0,0 +1,23 @@
+package restaurant.com.restaurant.menu.model;
+
+import jakarta.persistence.*;
+import restaurant.com.restaurant.menuitem.model.MenuItem;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "menu")
+    private Set<MenuItem> menuItems;
+}
