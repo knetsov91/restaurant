c33f3146ecfafb524c535e34e02bf38ec97ff61d
Konstantin <knetsov91@gmail.com>
2025-03-28 23:57:35 +0200

    Add fields in Menu entity.

--- restaurant/src/main/java/restaurant/com/restaurant/menu/model/Menu.java
+++ restaurant/src/main/java/restaurant/com/restaurant/menu/model/Menu.java
@@ -1,26 +1,32 @@
 package restaurant.com.restaurant.menu.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import restaurant.com.restaurant.menuitem.model.MenuItem;
 import restaurant.com.restaurant.restaurant.model.Restaurant;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 
+@Data
 @Entity
 public class Menu {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "menu")
-    private Set<MenuItem> menuItems;
+    private String title;
+
+    private String imagePath;
+
+    @OneToMany(mappedBy = "menu", fetch = FetchType.EAGER)
+    private Set<MenuItem> menuItems = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name="restaurant_id")
