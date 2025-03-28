2e42ecb12508da04c48cb6c590537b802f3a85e1
Konstantin <knetsov91@gmail.com>
2025-03-28 18:54:06 +0200

    Add field in Restaurant entity to connect with Menu entity.

--- restaurant/src/main/java/restaurant/com/restaurant/restaurant/model/Restaurant.java
+++ restaurant/src/main/java/restaurant/com/restaurant/restaurant/model/Restaurant.java
@@ -2,8 +2,11 @@ package restaurant.com.restaurant.restaurant.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 import restaurant.com.restaurant.kitchen.model.Kitchen;
+import restaurant.com.restaurant.menu.model.Menu;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Entity
@@ -23,4 +26,8 @@ public class Restaurant {
 
     @OneToOne(mappedBy = "restaurant")
     private Kitchen kitchen;
+
+    @OneToMany(mappedBy = "restaurant")
+    @ToString.Exclude
+    private List<Menu> menus;
 }
