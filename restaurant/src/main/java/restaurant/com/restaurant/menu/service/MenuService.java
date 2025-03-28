b22acd5e3fbe31096228f4f093bcc91a84527b6c
Konstantin <knetsov91@gmail.com>
2025-03-28 12:16:22 +0200

    Rename panel.html to admin-panel.html.

---
+++ restaurant/src/main/java/restaurant/com/restaurant/menu/service/MenuService.java
@@ -0,0 +1,30 @@
+package restaurant.com.restaurant.menu.service;
+
+import org.springframework.stereotype.Service;
+import restaurant.com.restaurant.menu.model.Menu;
+import restaurant.com.restaurant.menu.repository.MenuRepository;
+import restaurant.com.restaurant.restaurant.model.Restaurant;
+import restaurant.com.restaurant.restaurant.service.RestaurantService;
+import java.time.LocalDateTime;
+
+@Service
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final RestaurantService restaurantService;
+
+    public MenuService(MenuRepository menuRepository, RestaurantService restaurantService) {
+        this.menuRepository = menuRepository;
+        this.restaurantService = restaurantService;
+    }
+
+    public void createMenu(Long restaurantId) {
+        Restaurant restaurant = restaurantService.getRestaurantById(restaurantId);
+
+        Menu menu = new Menu();
+        menu.setRestaurant(restaurant);
+        menu.setCreatedAt(LocalDateTime.now());
+
+        menuRepository.save(menu);
+    }
+}
