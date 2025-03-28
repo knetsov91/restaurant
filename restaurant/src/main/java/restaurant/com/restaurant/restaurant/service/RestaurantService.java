39a5b6ceb99ee31cba173ac57298fd9f4ae67da1
Konstantin <knetsov91@gmail.com>
2025-03-28 10:28:05 +0200

    Add createRestaurant() in RestaurantService.

--- restaurant/src/main/java/restaurant/com/restaurant/restaurant/service/RestaurantService.java
+++ restaurant/src/main/java/restaurant/com/restaurant/restaurant/service/RestaurantService.java
@@ -3,6 +3,8 @@ package restaurant.com.restaurant.restaurant.service;
 import org.springframework.stereotype.Service;
 import restaurant.com.restaurant.restaurant.model.Restaurant;
 import restaurant.com.restaurant.restaurant.repository.RestaurantRepository;
+import restaurant.com.restaurant.web.dto.CreateRestaurantRequest;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -12,6 +14,15 @@ public class RestaurantService {
 
     public RestaurantService(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
+    }
+
+    public void createRestaurant(CreateRestaurantRequest createRestaurantRequest) {
+        Restaurant restaurant = new Restaurant();
+        restaurant.setCreatedAt(LocalDate.now());
+        restaurant.setName(createRestaurantRequest.getName());
+        restaurant.setAddress(createRestaurantRequest.getAddress());
+        restaurant.setCapacity(createRestaurantRequest.getCapacity());
+        restaurantRepository.save(restaurant);
     }
 
     public List<Restaurant> getRestaurants() {
