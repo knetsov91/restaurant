82bff0d1fe42f237da966a578fc83404361543fc
Konstantin <knetsov91@gmail.com>
2025-03-28 08:28:56 +0200

    Add GET /home endpoint.

--- restaurant/src/main/java/restaurant/com/restaurant/web/IndexController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/IndexController.java
@@ -1,21 +1,27 @@
 package restaurant.com.restaurant.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import restaurant.com.restaurant.config.AuthenticatedUser;
+import restaurant.com.restaurant.user.model.UserRole;
 
 @Controller
-
 public class IndexController {
 
     @GetMapping
     public String index() {
         return "index";
     }
+ 
+    @GetMapping("/home")
+    public String home(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        UserRole userRole = authenticatedUser.getUserRole();
 
-    @GetMapping("/dashboard")
-    public String dashboard() {
-
-        return "dashboard";
+        if (userRole == UserRole.ADMIN) {
+            return "redirect:/panel";
+        }
+        return "home";
     }
+
 }
