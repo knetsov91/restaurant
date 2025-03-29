c50348ff209cc2e76c97368af2c3566a0650ce31
Konstantin <knetsov91@gmail.com>
2025-03-29 11:22:01 +0200

    Add mapCreateUserRequestToUser() to DtoMapper.

--- restaurant/src/main/java/restaurant/com/restaurant/web/mapper/DtoMapper.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/mapper/DtoMapper.java
@@ -1,6 +1,7 @@
 package restaurant.com.restaurant.web.mapper;
 
 import restaurant.com.restaurant.employeeInfo.model.EmployeeInfo;
+import restaurant.com.restaurant.user.model.User;
 import restaurant.com.restaurant.web.dto.CreateEmployeeInfoRequest;
 import restaurant.com.restaurant.web.dto.CreateEmployeeRequest;
 import restaurant.com.restaurant.web.dto.CreateUserRequest;
@@ -24,6 +25,16 @@ public class DtoMapper {
                 .builder()
                 .age(createEmployeeInfoRequest.getAge())
                 .salary(createEmployeeInfoRequest.getSalary())
+                .build();
+    }
+
+    public static User mapCreateUserRequestToUser(CreateUserRequest createUserRequest) {
+        return User
+                .builder()
+                .firstName(createUserRequest.getFirstName())
+                .lastName(createUserRequest.getLastName())
+                .email(createUserRequest.getEmail())
+                .password(createUserRequest.getPassword())
                 .build();
     }
 }
