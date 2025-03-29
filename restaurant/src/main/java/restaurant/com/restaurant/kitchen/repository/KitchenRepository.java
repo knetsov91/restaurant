8daaab540e474b929554c6906f44f9bb6b78ff93
Konstantin <knetsov91@gmail.com>
2025-03-29 19:24:43 +0200

    Remove files.

--- restaurant/src/main/java/restaurant/com/restaurant/kitchen/repository/KitchenRepository.java
+++
@@ -1,9 +0,0 @@
-package restaurant.com.restaurant.kitchen.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import restaurant.com.restaurant.kitchen.model.Kitchen;
-
-@Repository
-public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
-}
