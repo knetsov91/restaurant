122af7d730dabdb016c4b0abbbe237b7a5350076
Konstantin <knetsov91@gmail.com>
2025-03-30 20:48:44 +0300

    Add GET /menus/create to render menu-create.html Thymeleaf template.

--- restaurant/src/main/java/restaurant/com/restaurant/web/MenuController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/MenuController.java
@@ -1,21 +1,25 @@
 package restaurant.com.restaurant.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import restaurant.com.restaurant.menu.model.Menu;
 import restaurant.com.restaurant.menu.service.MenuService;
+import restaurant.com.restaurant.restaurant.model.Restaurant;
+import restaurant.com.restaurant.restaurant.service.RestaurantService;
+import restaurant.com.restaurant.web.dto.CreateMenuRequest;
 import java.util.List;
 
 @Controller
 public class MenuController {
 
     private MenuService menuService;
+    private RestaurantService restaurantService;
 
-    public MenuController(MenuService menuService) {
+    public MenuController(MenuService menuService, RestaurantService restaurantService) {
         this.menuService = menuService;
+        this.restaurantService = restaurantService;
     }
 
     @GetMapping("/menus")
@@ -30,12 +34,24 @@ public class MenuController {
     }
 
     @GetMapping("/restaurants/{restaurantId}/menus")
-    public ModelAndView getAllRestaurantMenus(@PathVariable(name = "restaurantId") Long restaurantId, Model model) {
+    public ModelAndView getAllRestaurantMenus(@PathVariable(name = "restaurantId") Long restaurantId) {
 
         List<Menu> menus = menuService.getMenusByRestaurantId(restaurantId);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("menus", menus);
         modelAndView.setViewName("menus");
+        return modelAndView;
+    }
+
+    @GetMapping("/menus/create")
+    public ModelAndView getCreateMenu() {
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("menu-create");
+        modelAndView.addObject("createMenuRequest", new CreateMenuRequest());
+        modelAndView.addObject("restaurants", restaurants);
+
         return modelAndView;
     }
 
