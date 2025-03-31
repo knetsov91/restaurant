6066cc1b2c101e5cf1f599c85d89ba65c122999a
Konstantin <knetsov91@gmail.com>
2025-03-31 09:05:49 +0300

    Add catch all exception handling in GlobalExceptionHandler.

--- restaurant/src/main/java/restaurant/com/restaurant/exception/GlobalExceptionHandler.java
+++ restaurant/src/main/java/restaurant/com/restaurant/exception/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package restaurant.com.restaurant.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handle(Exception e) {
         return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleForbidden(AccessDeniedException e) {
+        log.error(e.getMessage());
+        log.error(e.getClass().getName());
+        return "redirect:/home";
     }
 }
