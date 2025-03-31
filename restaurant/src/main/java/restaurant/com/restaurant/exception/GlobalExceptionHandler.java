00dfa44df8353242a558e64298524813025ddb82
Konstantin <knetsov91@gmail.com>
2025-03-31 10:24:52 +0300

    Modify in ControllerAdvice the method that handle general exceptions to render view.

--- restaurant/src/main/java/restaurant/com/restaurant/exception/GlobalExceptionHandler.java
+++ restaurant/src/main/java/restaurant/com/restaurant/exception/GlobalExceptionHandler.java
@@ -1,19 +1,27 @@
 package restaurant.com.restaurant.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handle(Exception e) {
-        return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ModelAndView handle(Exception e, HttpServletRequest request) {
+        log.error(e.toString());
+
+        String url = request.getRequestURL().toString();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error-page");
+        modelAndView.addObject("url", url);
+
+        return modelAndView;
     }
 
     @ExceptionHandler(AccessDeniedException.class)
