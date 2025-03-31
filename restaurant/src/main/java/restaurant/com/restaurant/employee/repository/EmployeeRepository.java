9eae3489c022010e098f12dc5b3694ac2bf2f466
Konstantin <knetsov91@gmail.com>
2025-03-31 15:43:32 +0300

    Add findByUser() in EmployeeRepository.

--- restaurant/src/main/java/restaurant/com/restaurant/employee/repository/EmployeeRepository.java
+++ restaurant/src/main/java/restaurant/com/restaurant/employee/repository/EmployeeRepository.java
@@ -3,8 +3,11 @@ package restaurant.com.restaurant.employee.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import restaurant.com.restaurant.employee.model.Employee;
+import restaurant.com.restaurant.user.model.User;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    Optional<Employee> findByUser(User user);
 }
