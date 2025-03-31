46681a11a55d9345ed4cdf5fb0be3a5e880a4acc
Konstantin <knetsov91@gmail.com>
2025-03-31 13:19:41 +0300

    Change EmployeeRepository's key generic parameter to UUID.

--- restaurant/src/main/java/restaurant/com/restaurant/employee/repository/EmployeeRepository.java
+++ restaurant/src/main/java/restaurant/com/restaurant/employee/repository/EmployeeRepository.java
@@ -3,7 +3,8 @@ package restaurant.com.restaurant.employee.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import restaurant.com.restaurant.employee.model.Employee;
+import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
