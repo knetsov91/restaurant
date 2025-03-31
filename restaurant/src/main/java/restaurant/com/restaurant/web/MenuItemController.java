e6089fda3c77cc63a9c6d6b904511939f3590ae3
Konstantin <knetsov91@gmail.com>
2025-03-31 08:43:32 +0300

    Add GET endpoint in MenuItemController to render template for adding menu item to menu.

--- restaurant/src/main/java/restaurant/com/restaurant/web/MenuItemController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/MenuItemController.java
@@ -5,26 +5,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import restaurant.com.restaurant.menuitem.model.MenuItem;
 import restaurant.com.restaurant.menuitem.service.MenuItemService;
+import restaurant.com.restaurant.restaurant.model.Restaurant;
+import restaurant.com.restaurant.restaurant.service.RestaurantService;
 import restaurant.com.restaurant.web.dto.CreateMenuItemRequest;
 import java.util.List;
 
 @Controller
-@RequestMapping("/menu-items")
 public class MenuItemController {
 
     private MenuItemService menuItemService;
+    private RestaurantService restaurantService;
 
     @Autowired
-    public MenuItemController(MenuItemService menuItemService) {
+    public MenuItemController(MenuItemService menuItemService, RestaurantService restaurantService, RestaurantService restaurantService1) {
         this.menuItemService = menuItemService;
+        this.restaurantService = restaurantService1;
     }
 
-    @GetMapping
+    @GetMapping("/menu-items")
     public ModelAndView getItems() {
         List<MenuItem> items = menuItemService.getAllMenuItems();
         ModelAndView modelAndView = new ModelAndView();
@@ -35,21 +38,34 @@ public class MenuItemController {
 
     @GetMapping("/create")
     public ModelAndView createItem() {
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("menu-item-create");
         modelAndView.addObject("items", List.of());
+        modelAndView.addObject("restaurants", restaurants);
         modelAndView.addObject("createMenuItemRequest", CreateMenuItemRequest.builder().build());
         return modelAndView;
     }
 
-    @PostMapping
+    @PostMapping("/menu-items")
     public ModelAndView postItems(@Valid CreateMenuItemRequest createMenuItemRequest, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         if (bindingResult.hasErrors()) {
-                    return new ModelAndView("menu-item-create");
+            return new ModelAndView("menu-item-create");
         }
 
         menuItemService.createMenuItem(createMenuItemRequest);
         return new ModelAndView("redirect:/menu-items");
+    }
+
+    @GetMapping("/menu/{menuId}/menu-items/create")
+    public ModelAndView createMenuItem(@PathVariable Long menuId) {
+        List<MenuItem> allMenuItems = menuItemService.getAllMenuItems();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("menu-add-item");
+        modelAndView.addObject("allMenuItems", allMenuItems);
+        modelAndView.addObject("menuId", menuId);
+        return modelAndView;
     }
 }
