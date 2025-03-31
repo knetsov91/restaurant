dccc8b80b7e49adc99e3fca850a4a918b6991144
Konstantin <knetsov91@gmail.com>
2025-03-31 08:45:57 +0300

    Add addMenuItem() in MenuItemService.

--- restaurant/src/main/java/restaurant/com/restaurant/menuitem/service/MenuItemService.java
+++ restaurant/src/main/java/restaurant/com/restaurant/menuitem/service/MenuItemService.java
@@ -2,20 +2,25 @@ package restaurant.com.restaurant.menuitem.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import restaurant.com.restaurant.menu.model.Menu;
+import restaurant.com.restaurant.menu.service.MenuService;
 import restaurant.com.restaurant.menuitem.model.MenuItem;
 import restaurant.com.restaurant.menuitem.repository.MenuItemRepository;
 import restaurant.com.restaurant.web.dto.CreateMenuItemRequest;
 import restaurant.com.restaurant.web.mapper.DtoMapper;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MenuItemService {
 
     private final MenuItemRepository menuItemRepository;
+    private MenuService menuService;
 
     @Autowired
-    public MenuItemService(MenuItemRepository menuItemRepository) {
+    public MenuItemService(MenuItemRepository menuItemRepository, MenuService menuService) {
         this.menuItemRepository = menuItemRepository;
+        this.menuService = menuService;
     }
 
     public List<MenuItem> getAllMenuItems() {
@@ -26,5 +31,14 @@ public class MenuItemService {
         MenuItem mapped = DtoMapper.mapCreateMenuItemRequestToMenuItem(menuItem);
 
         menuItemRepository.save(mapped);
+    }
+
+    public  void addMenuItem(UUID menuItemId, Long menuId) {
+        Menu menu = menuService.getMenuById(menuId);
+
+        MenuItem menuItem = menuItemRepository.findById(menuItemId).orElseThrow(() -> new RuntimeException("No menu item found with id: " + menuItemId));
+        menuItem.setMenu(menu);
+
+        menuItemRepository.save(menuItem);
     }
 }
