b8682532635ff329e7517dff17c017971b423f62
Konstantin <knetsov91@gmail.com>
2025-04-01 17:22:56 +0300

    Add GET /reservations in ReservationController.

--- restaurant/src/main/java/restaurant/com/restaurant/web/ReservationController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/ReservationController.java
@@ -3,11 +3,14 @@ package restaurant.com.restaurant.web;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import restaurant.com.restaurant.reservation.model.Reservation;
 import restaurant.com.restaurant.reservation.service.ReservationService;
 import restaurant.com.restaurant.web.dto.CreateReservationRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/reservations")
@@ -27,5 +30,16 @@ public class ReservationController {
         }
         reservationService.createReservation(createReservationRequest);
         return new ModelAndView("redirect:/");
+    }
+
+    @GetMapping
+    public ModelAndView getAllReservations() {
+        List<Reservation> reservations = reservationService.getAllReservations();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("reservation/reservations");
+        modelAndView.addObject("reservations", reservations);
+
+        return modelAndView;
     }
 }
