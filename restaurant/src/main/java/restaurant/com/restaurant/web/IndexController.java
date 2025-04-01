eb0004aef8a1e0ae1b7ded18737207c233ee0b21
Konstantin <knetsov91@gmail.com>
2025-04-01 16:35:55 +0300

    Add list of restaurants to the model in IndexController's index().

--- restaurant/src/main/java/restaurant/com/restaurant/web/IndexController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/IndexController.java
@@ -3,15 +3,31 @@ package restaurant.com.restaurant.web;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import restaurant.com.restaurant.config.AuthenticatedUser;
+import restaurant.com.restaurant.restaurant.model.Restaurant;
+import restaurant.com.restaurant.restaurant.service.RestaurantService;
 import restaurant.com.restaurant.user.model.UserRole;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private final RestaurantService restaurantService;
+
+    public IndexController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
     @GetMapping
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("restaurants", restaurants);
+
+        return modelAndView;
     }
 
     @GetMapping("/panel")
