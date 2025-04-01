879e1e8a153f9d3d2f6252a1542ef05cee0157af
Konstantin <knetsov91@gmail.com>
2025-04-01 15:11:38 +0300

    Add @EnableMethodSecurity and @EnableFeignClients in RestaurantApplication.

--- restaurant/src/main/java/restaurant/com/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/restaurant/com/restaurant/RestaurantApplication.java
@@ -2,8 +2,12 @@ package restaurant.com.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
+@EnableFeignClients
 public class RestaurantApplication {
 
     public static void main(String[] args) {
