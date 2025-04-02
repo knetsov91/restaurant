cbef34aefb552f5a085d1280cfa0c3019e9635e2
Konstantin <knetsov91@gmail.com>
2025-04-02 20:48:50 +0300

    Add getOrdersByRestaurantId() in OrderClient.

--- restaurant/src/main/java/restaurant/com/restaurant/order/client/OrderClient.java
+++ restaurant/src/main/java/restaurant/com/restaurant/order/client/OrderClient.java
@@ -3,6 +3,7 @@ package restaurant.com.restaurant.order.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import restaurant.com.restaurant.order.client.dto.OrderResponse;
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface OrderClient {
 
     @GetMapping
     ResponseEntity<List<OrderResponse>> getOrders();
+
+    @GetMapping("/restaurants/{restaurantId}")
+    ResponseEntity<List<OrderResponse>> getOrdersByRestaurantId(@PathVariable Long restaurantId);
 }
