c88689c25a4a3e836002a41ffed50f05c178a665
Konstantin <knetsov91@gmail.com>
2025-04-03 18:54:46 +0300

    Add validation in CreateMenuRequest.

--- restaurant/src/main/java/restaurant/com/restaurant/web/dto/CreateMenuRequest.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/dto/CreateMenuRequest.java
@@ -1,10 +1,15 @@
 package restaurant.com.restaurant.web.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateMenuRequest {
 
+    @NotEmpty(message = "Enter menu name")
     private String title;
+
+    @NotNull
     private Long restaurantId;
 }
