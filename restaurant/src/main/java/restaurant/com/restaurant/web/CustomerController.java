74187ce5c4cb2d8234aea00d4b95f1355cebb7fd
Konstantin <knetsov91@gmail.com>
2025-04-03 12:12:49 +0300

    Add home() GET handler in CustomerController.

--- restaurant/src/main/java/restaurant/com/restaurant/web/CustomerController.java
+++ restaurant/src/main/java/restaurant/com/restaurant/web/CustomerController.java
@@ -3,15 +3,31 @@ package restaurant.com.restaurant.web;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import restaurant.com.restaurant.config.AuthenticatedUser;
+import restaurant.com.restaurant.customer.model.Customer;
+import restaurant.com.restaurant.customer.service.CustomerService;
+import restaurant.com.restaurant.reservation.service.ReservationService;
 
 @Controller
 public class CustomerController {
 
-    @GetMapping("/home")
-    public String home(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+    private ReservationService reservationService;
+    private CustomerService customerService;
 
-        return "home";
+    public CustomerController(ReservationService reservationService, CustomerService customerService) {
+        this.reservationService = reservationService;
+        this.customerService = customerService;
     }
 
+    @GetMapping("/home")
+    public ModelAndView home(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        Customer customerByUserId = customerService.getCustomerByUserId(authenticatedUser.getUserId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("customerId", customerByUserId.getId());
+        modelAndView.setViewName("home");
+
+        return modelAndView;
+    }
 }
