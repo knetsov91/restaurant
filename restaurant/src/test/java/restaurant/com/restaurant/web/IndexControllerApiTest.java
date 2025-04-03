95d074619cba4ec0c9abe19450fe65936219b11d
Konstantin <knetsov91@gmail.com>
2025-04-03 17:57:51 +0300

    Add getAuthenticatedRequestForAdminToPanelEndpoint_shouldRenderAdminPanel() in IndexControllerApiTest.

--- restaurant/src/test/java/restaurant/com/restaurant/web/IndexControllerApiTest.java
+++ restaurant/src/test/java/restaurant/com/restaurant/web/IndexControllerApiTest.java
@@ -7,13 +7,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import restaurant.com.restaurant.TestBuilder;
+import restaurant.com.restaurant.config.AuthenticatedUser;
 import restaurant.com.restaurant.restaurant.model.Restaurant;
 import restaurant.com.restaurant.restaurant.service.RestaurantService;
-
+import restaurant.com.restaurant.user.model.UserRole;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(IndexController.class)
@@ -36,5 +37,19 @@ class IndexControllerApiTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("restaurants"));
+    }
+
+    @Test
+    void getAuthenticatedRequestForAdminToPanelEndpoint_shouldRenderAdminPanel() throws Exception {
+        AuthenticatedUser authenticatedUser = TestBuilder.createAuthenticatedUser();
+        authenticatedUser.setUserRole(UserRole.ADMIN);
+
+        MockHttpServletRequestBuilder req = MockMvcRequestBuilders.get("/panel")
+                .with(user(authenticatedUser));
+
+        mockMvc.perform(req)
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin-panel"));
+
     }
 }
