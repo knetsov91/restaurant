44c99ac6c89f53ff56b77044154787792fee398f
Konstantin <knetsov91@gmail.com>
2025-04-03 11:57:39 +0300

    Add getRequestToRegisterEndpoint_shouldReturnRegisterView() in AuthenticationControllerApiTest.

--- restaurant/src/test/java/restaurant/com/restaurant/web/AuthenticationControllerApiTest.java
+++ restaurant/src/test/java/restaurant/com/restaurant/web/AuthenticationControllerApiTest.java
@@ -1,31 +1,23 @@
 package restaurant.com.restaurant.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import restaurant.com.restaurant.exception.RegisterException;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import restaurant.com.restaurant.user.service.UserService;
 import restaurant.com.restaurant.web.dto.RegisterRequest;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doThrow;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(AuthenticationController.class)
-//@ExtendWith(MockitoExtension.class)
-class AuthenticationControllerTest {
+class AuthenticationControllerApiTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -33,10 +25,6 @@ class AuthenticationControllerTest {
     @MockitoBean
     private UserService userService;
 
-    @Autowired
-    AuthenticationControllerTest(UserService userService) {
-        this.userService = userService;
-    }
 
     @Test
     void testRegistration_whenUserExists_thenThrowsException() throws Exception {
@@ -45,17 +33,25 @@ class AuthenticationControllerTest {
                 .password("password")
                 .passwordConfirmation("password")
                 .build();
-        doThrow(RegisterException.class).when(userService).register(build);
 
         MvcResult mvcResult = mockMvc.perform(
                 post("/register")
                         .param("email", email)
                         .param("password", "password")
                         .param("passwordConfirmation", "password")
-        ).andExpect(status().isOk()).andReturn();
+                        .with(csrf())
+        ).andExpect(status().is3xxRedirection()).andReturn();
 
         MockHttpServletResponse response = mvcResult.getResponse();
 
     }
+    @Test
+    void getRequestToRegisterEndpoint_shouldReturnRegisterView() throws Exception {
 
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/register");
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"));
+    }
 }
