8ec5f7c83b0fcf5c9c1737b7531cdd9297a3b241
Konstantin <knetsov91@gmail.com>
2025-04-03 12:13:30 +0300

    Add findByUserId() in CustomerRepository.

--- restaurant/src/main/java/restaurant/com/restaurant/customer/repository/CustomerRepository.java
+++ restaurant/src/main/java/restaurant/com/restaurant/customer/repository/CustomerRepository.java
@@ -3,7 +3,10 @@ package restaurant.com.restaurant.customer.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import restaurant.com.restaurant.customer.model.Customer;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+    Optional<Customer> findByUserId(UUID userId);
 }
