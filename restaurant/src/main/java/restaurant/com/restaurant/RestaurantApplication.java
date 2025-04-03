29ab5a840b6923bf0c477e80652e1b7c7220d988
Konstantin <knetsov91@gmail.com>
2025-04-03 13:34:37 +0300

    Enable scheduling in RestaurantApplication.

--- restaurant/src/main/java/restaurant/com/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/restaurant/com/restaurant/RestaurantApplication.java
@@ -3,8 +3,10 @@ package restaurant.com.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableMethodSecurity
 @EnableFeignClients
